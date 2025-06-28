028c3e7f5e95d22302c388785b9eae6e0f366359
karoney-k-noah <karoney.kn@gmail.com>
2025-06-28 22:50:54 +0300

    Virtual Functions

---
+++ labs/15. virtualfunctions.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class base     
+{
+public:
+    void display()   
+    {
+        cout << "\n display base";
+    }
+    virtual void show() 
+    {
+        cout << "\n show base";
+    }
+};
+
+class derived : public base
+{
+public:
+    void display()
+    {
+        cout << "\n display derived";
+    }
+    void show()
+    {
+        cout << "\n show derived";
+    }
+};
+
+int main()
+{
+    base b;      
+    derived d;   
+    base *bptr;  
+
+    cout << "\n bptr points to base \n";
+    bptr = &b;
+    bptr->display();
+    bptr->show();
+
+    cout << "\n\n bptr points to derived \n";
+    bptr = &d;
+    bptr->display();
+    bptr->show();
+
+    return 0;
+}
